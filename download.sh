9973e27d7970616b5bb8dce2d61a7dab11b4dfed
German Namestnikov <german.namestnikov@illegalbytes.com>
2021-04-22 00:01:23 +0300

    Update download.sh
    Changed newlib link

--- download.sh
+++ download.sh
@@ -3,4 +3,5 @@ mkdir src/ install/ build/
 
 wget http://ftp.gnu.org/gnu/gcc/gcc-4.5.2/gcc-4.5.2.tar.bz2 -O src/gcc-4.5.2.tar.bz2
 wget http://ftp.gnu.org/gnu/binutils/binutils-2.21.1a.tar.bz2 -O src/binutils-2.21.1a.tar.bz2
-wget ftp://sources.redhat.com/pub/newlib/newlib-1.19.0.tar.gz -O src/newlib-1.19.0.tar.gz
+#wget ftp://sources.redhat.com/pub/newlib/newlib-1.19.0.tar.gz -O src/newlib-1.19.0.tar.gz
+wget http://www.sourceware.org/pub/newlib/newlib-1.19.0.tar.gz -O src/newlib-1.19.0.tar.gz
